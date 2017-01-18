2423d91451846f31e1023845889b7fa7049dfc43
Freida Blostein <blostein@umich.edu>
2017-01-18 11:09:08 -0500

    Added Comment

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,5 +1,6 @@
 #this is how you comment 
 #always comment
+#forshoyo
 #This is a script that I wrote
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
